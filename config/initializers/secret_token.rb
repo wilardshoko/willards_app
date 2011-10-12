dace943c589cb8a3a731bbaad762e8201b071e2d
root <root@willard-TravelMate-4200.(none)>
2011-10-12 13:49:36 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cfb2f8a36281e034a688fcb8fed0db00e46c9653c35238350619c5b760834f3ca8ee5102dcdfd0546b235325cde8291471822e60ed1cf1ae4f1f84ed15dd5a28'
